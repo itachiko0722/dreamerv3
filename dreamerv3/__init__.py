c0f2b7bb439ecd9e2c74869062e912f355a508dd
itachiko0722 <m2116005ia@ug.jwu.ac.jp>
2025-05-07 03:50:33 +0900

    init.pyを編集

---
+++ dreamerv3/__init__.py
@@ -0,0 +1,28 @@
+# dreamerv3/dreamerv3/__init__.py
+
+"""
+dreamerv3
+
+CNN-Dreamer V3: A convolutional encoder version of the Dreamer algorithm.
+"""
+
+# 1) Package version
+__version__ = "0.1.0"
+
+# 2) Expose your main classes/functions at the package level
+#    Uncomment or adjust as you add modules:
+
+# from .agent        import DreamerV3Agent
+# from .world_model  import WorldModel
+# from .policy       import PolicyNetwork
+# from .replay       import ReplayBuffer
+# from .env_wrapper  import EnvWrapper
+
+# 3) Define what gets imported with `from dreamerv3 import *`
+__all__ = [
+    # "DreamerV3Agent",
+    # "WorldModel",
+    # "PolicyNetwork",
+    # "ReplayBuffer",
+    # "EnvWrapper",
+]
